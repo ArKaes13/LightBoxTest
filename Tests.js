aaf97f5e8ddba86e43a60fcafa7d8e3896f9d338
Tyler <ArKaes1820@yahoo.com>
2024-10-06 19:45:40 -0700

    added a second test

--- Tests.js
+++ Tests.js
@@ -1,7 +1,8 @@
-const {By, Builder} = require('selenium-webdriver')
+const {By, Builder, until} = require('selenium-webdriver')
+const axios = require('axios');
 
-async function lightBoxTest() {
-    const driver = await new Builder().forBrowser('firefox').build();
+async function lightBoxCareer() {
+    let driver = await new Builder().forBrowser('firefox').build();
     await driver.get('https://www.lightboxre.com/');
 
     //Clicks on 'about' section and then clicks on 'careers' section
@@ -23,4 +24,57 @@ async function lightBoxTest() {
     await driver.quit()
 }
 
-lightBoxTest();
+async function lightBoxAbout() {
+    let driver = await new Builder().forBrowser('firefox').build();
+    await driver.get('https://www.lightboxre.com/');
+
+    //Clicks on 'about' section and then clicks on 'careers' section
+    let about = await driver.findElement(By.xpath('//li[@id="menu-item-5"]'));
+    await about.click();
+    let career = await driver.findElement(By.xpath('//li[@id="menu-item-5"]/ul/li[6]'));
+    await career.click();
+
+    //Saves our current window id
+    let originalWindow = await driver.getWindowHandle();
+
+    //Switches into the iframe
+    driver.switchTo().frame(0);
+
+    //Clicks the 'Job Search' link
+    let jobSearch = await driver.findElement(By.xpath('//a[contains(text(), "Job Search")]'));
+    jobSearch.click();
+
+    // Loop through until we find a new window handle and switch to that new window
+    let windows = await driver.getAllWindowHandles();
+    windows.forEach(async handle => {
+        if (handle !== originalWindow) {
+        await driver.switchTo().window(handle);
+        }
+    });
+
+    //Locates the about us link and stores its href.
+    let aboutUs = await driver.findElement(By.xpath('//a[contains(text(), "About Us")]'))
+    let aboutUsLink = await aboutUs.getAttribute('href')
+
+    //Checks the link and returns the http status code.
+    try {
+        let response = await axios.get(aboutUsLink);
+        if (response.status == 200) {
+            console.log(`Test Passed: '${aboutUsLink}' was fulfilled.`);
+            console.log(`Http Status Code: ${response.status}`);
+        } else {
+            console.error(`Test Failed: '${aboutUsLink}' was unfulfilled.`);
+            console.error(`Http Status Code: ${response.status}`);
+        }
+    } catch (error) {
+            console.error(`'${aboutUsLink}' is not a valid URL.`);
+            console.error(`Axios error message: ${error.message}`);
+    }
+
+    // quits the test
+    await driver.quit()
+}
+
+//Runs both tests
+lightBoxCareer();
+lightBoxAbout();
