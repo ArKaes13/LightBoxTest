7dd3d6ee21225bf7257e6813b2296f259a8456d7
Tyler <ArKaes1820@yahoo.com>
2024-10-09 16:04:05 -0700

    fixed first test due to site change

--- Tests.js
+++ Tests.js
@@ -14,11 +14,46 @@ async function lightBoxCareer() {
     //Switches into the iframe
     driver.switchTo().frame(0);
 
-    //Finds the job posting, slices the job title to be more reasonable, and logs the result
-    let job = await driver.findElement(By.xpath('//body/div/div/div/main/div[3]/ul/li[2]/div/div/div/h2/a'))
+    //Clicks the 'Job Search' link
+    let jobSearch = await driver.findElement(By.xpath('//a[contains(text(), "Job Search")]'));
+    jobSearch.click();
+
+    //Waits for the new tab to open, grabs all tabs, and then switches to the new one
+    await driver.sleep(1000);
+    let tabs = await driver.getAllWindowHandles();
+    await driver.switchTo().window(tabs[1]);
+
+    //Waits for the apply link to appear and then clicks on it
+    let apply = await driver.wait(until.elementLocated(By.xpath('//a[contains(text(), "Apply")]')), 20000);
+    apply.click();
+
+    //Finds the input fields/button, inputs login information, and logs in
+    let email = await driver.wait(until.elementLocated(By.xpath('//input[@type="email"]')), 20000);
+    await email.sendKeys('ArKaes1820@yahoo.com');
+    let password = await driver.findElement(By.xpath('//input[@type="password"]'));
+    await password.sendKeys('TestPassword1820!');
+    let loginBtn = await driver.findElement(By.xpath('//button[@name="login"]'));
+    await loginBtn.click();
+
+    //Waits for the login to finish, finds the job search link, and clicks it.
+    await driver.sleep(3000);
+    let jobSearch2 = await driver.wait(until.elementLocated(By.xpath('//a[contains(text(), "Job Search")]')), 10000);
+    await jobSearch2.click();
+
+    //Waits for page to load, finds the applied jobs link, and clicks it.
+    await driver.sleep(3000);
+    let appliedJobs = await driver.wait(until.elementLocated(By.xpath('//a[contains(text(), "Applied Jobs")]')), 10000);
+    appliedJobs.click();
+
+
+    //Finds the job posting/status, slices the job title to be more reasonable, and logs the result
+    await driver.sleep(3000);
+    let job = await driver.findElement(By.xpath('//div[@class="posting-title"]/h2/a'))
     let jobTitle = await job.getText();
+    let status = await driver.findElement(By.xpath('//div[@class="status"]/div'))
+    let statusText = await status.getText();
     jobTitle = jobTitle.slice(33, jobTitle.length)
-    console.log(`Tyler would be perfect for the role of '${jobTitle}'!`)
+    console.log(`Tyler would be perfect for the role of '${jobTitle}' and was definitely not '${statusText}!`)
 
     //quits the test
     await driver.quit()
@@ -67,7 +102,7 @@ async function lightBoxAbout() {
             console.error(`Http Status Code: ${response.status}`);
         }
     } catch (error) {
-            console.error(`'${aboutUsLink}' is not a valid URL.`);
+            console.error(`Test Failed: '${aboutUsLink}' is not a valid URL.`);
             console.error(`Axios error message: ${error.message}`);
     }
 
